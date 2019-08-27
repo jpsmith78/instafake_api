4ac5a1204f904c8ff8820726027d15f12cebb394
Justin Smith <justinpetersmith@gmail.com>
2019-08-27 12:53:12 -0400

    added sessions controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,45 @@
 class PhotosController < ApplicationController
 
+
   def index
-    render json: Photo.all
+    render json: Photo.all.as_json(include: [:user])
   end
+
+  def show
+    render json: Photo.find(params[:id]).as_json(include: [:user])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update(photo_params)
+      render json: @photo
+    else
+      render json: { :errors => @photo.errors.full_messages }
+    end
+
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.user = User.find(session[:user_id])
+    if @photo.save
+      render json: @photo
+    else
+      render json: { :errors => @photo.errors.full_messages }
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    render json: {status: "destroyed #{@photo.title}"}
+  end
+
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :picture, :description)
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,37 @@
 class UsersController < ApplicationController
 
   def index
-    render json: User.all
+    render json: User.all.as_json(include: [:photos])
   end
+
+  def show
+    render json: User.find(params[:id]).as_json(include: [:photos])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    render json: @user
+
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: {status: "destroyed #{@user.username}"}
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 
 end
